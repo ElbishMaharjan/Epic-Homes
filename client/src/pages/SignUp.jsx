287249570d0896080506697fe1f9dc01aa6c5abc
ElbishMaharjan <elbishmaharjan9@gmail.com>
2024-03-16 23:54:22 +0545

    Complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,56 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});   // creating object and function setformdata, import usestate,ckeep track of all data.
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {            //  event listener call function handlechange, and e-take an event
+    setFormData(        
+      {
+        ...formData, //help ko keep previous data by using spread operator(...)
+        [e.target.id]: e.target.value, //add new changes with the help of id and what ever is change set that value
+      });
+  };
+  const handleSubmit = async(e) => {       //creating functiom
+    e.preventDefault();                //to prevent the refreshing when we submit the form
+    try {
+      setLoading(true);
+    const res = await fetch('/api/auth/signup',     // req localhost 
+    {
+      method: 'POST',       //adding object
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),    //send the body from the browser and send this (formdata)
+    });
+    const data = await res.json();        //change and convert response we get to JSON so we can use it and see it.
+    console.log(data);         // if user is new and enters correct information "user created sucessfully" is dispalyed
+    if (data.success === false){
+      setLoading(false);
+      setError(data.message);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
+    } catch(error){
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text text-center font-semibold my-7'>Sign Up</h1>
-    <form className='flex flex-col gap-4'>
-      <input type = "text" placeholder='username' className='border p-3 rounded-lg' id='username'/> 
-      <input type = "email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
-      <input type = "password" placeholder='password' className='border p-3 rounded-lg' id='password'/>
-      <button className='bg-orange-600 text-white p-3 rounded-lg uppercase hover:bg-black   opacity-85 disabled:opacity-80'>Sign up</button>
+    <form onSubmit= {handleSubmit} className='flex flex-col gap-4'>    
+      <input type = "text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/> 
+      <input type = "email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+      <input type = "password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange} />
+
+      <button disabled = {loading} className='bg-orange-600 text-white p-3 rounded-lg uppercase hover:bg-black   opacity-85 disabled:opacity-80'>{loading ? 'Loading...': 'sign Up'}</button>
     </form>
     <div className="flex gap-2 mt-5">
       <p>Do you Have an account?</p>
@@ -17,6 +59,7 @@ export default function SignUp() {
       </Link>
 
     </div>
+    {(error) && <p className='text-orange-600 mt-5'>{error}</p>}
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {   
+  proxy: {    //proxy allows you to redirect certain requests to a different server during development. This is useful, for example, when your frontend application needs to communicate with a backend API running on a separate server.
+    '/api': {  // each time the address starts with /api add the localhost 3000 at the beginning
+      target: 'http://localhost:3000',
+      secure: false,
+    },
+  },
+},
+
   plugins: [react()],
 })
