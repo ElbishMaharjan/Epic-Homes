baa430aaa94a3aaf9df0480b8ea4b7eb802c422a
ElbishMaharjan <elbishmaharjan9@gmail.com>
2024-04-04 02:42:08 +0545

    Create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -12,7 +12,7 @@ export const createListing = async (req, res, next) => {   // Define an asynchro
 
 // Controller function to handle the deletion of a listing
 export const deleteListing = async (req, res, next) => {  
-   const listing = await Listing.findById(req.params.id);   //Checking if the listing exists or not, from our lsiting model by using findebyID,  This function retrieves the listing to be deleted by its ID from the database
+   const listing = await Listing.findById(req.params.id);   //Checking if the listing exists or not, from our lisiting model by using findebyID,  This function retrieves the listing to be deleted by its ID from the database
    
    if (!listing) {    // If the listing does not exist, invoke an error handler with status code 404
       return next(errorHandler(404, 'Listing not found!'));
@@ -25,6 +25,28 @@ export const deleteListing = async (req, res, next) => {
    try {
       await Listing.findByIdAndDelete(req.params.id);  // If authentication and validation are successful, delete the listing from the database
       res.status(200).json('Listing has been deleted!');  // Send a success message in the response with status code 200
+   } catch (error) {
+      next(error);           // If an error occurs during the deletion process, pass it to the error handling middleware
+   }
+};
+
+// Controller function to handle the updating of a listing
+export const updateListing = async (req, res, next) => {
+   const listing = await Listing.findById(req.params.id);  //Retrieve the listing to be updated by its ID from the database
+   if (!listing) {    // If the listing does not exist, invoke an error handler with status code 404
+      return next(errorHandler(404, 'Listing not found!'));
+   }
+   
+   if (req.user.id !== listing.userRef) {        // If the authenticated user ID does not match the user ID associated with the listing, invoke an error handler with status code 401, checking if user is owner of the listing or not
+      return next(errorHandler(401, 'you can only update your own listings!'));
+   }
+   try {     // Update the listing with the provided data using the 'findByIdAndUpdate' method
+     const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,      // 1. The ID of the listing to be updated (req.params.id)
+      req.body,           // 2. The new data to be applied to the listing (req.body)
+      {new: true}         // 3. An options object specifying that the updated document should be returned (new: true)
+   );
+      res.status(200).json(updatedListing);  // Send a success message in the response with status code 200
    } catch (error) {
       next(error);           // If an error occurs during the deletion process, pass it to the error handling middleware
    }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from 'express';                                        // Import the Express framework
-import { createListing, deleteListing } from '../controllers/listing.controller.js'; // Import the createListing function from the listing.controller module
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js'; // Import the createListing function from the listing.controller module
 import { verifyToken } from '../utils/verifyUser.js';                 // Import the verifyToken function from the verifyUser module
 
 const router = express.Router();                      // Create a router instance using Express
 
 router.post('/create', verifyToken, createListing);   // Define a route for creating a new listing , This route handles POST requests to /create endpoint, The verifyToken middleware is used to verify the user's token before allowing access, The createListing function from the controller is invoked when the route is accessed
 router.delete('/delete/:id', verifyToken, deleteListing); // Define a route for handling DELETE requests to delete a listing by ID,This route expects a dynamic parameter ':id' in the URL path, representing the unique identifier of the listing to be deleted,The 'verifyToken' middleware is used to authenticate the request and ensure the user has permission to delete the listing, Upon successful authentication, the 'deleteListing' controller function is called to handle the deletion process
-
+router.post('/update/:id', verifyToken, updateListing);  // Define a route for handling POST requests to update a listing by ID,This route expects a dynamic parameter ':id' in the URL path, representing the unique identifier of the listing to be updated,The 'verifyToken' middleware is used to authenticate the request and ensure the user has permission to update the listing,Upon successful authentication, the 'updateListing' controller function is called to handle the updating process
 export default router;                                // Export the router instance to be used in other modules
